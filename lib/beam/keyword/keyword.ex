426b454ffe946293b2c5633d68c02aa92b59ece4
Emilano <azadvararvand@gmail.com>
2021-12-27 13:41:15 +0330

    Add files via upload

--- lib/beam/keyword/keyword.ex
+++ lib/beam/keyword/keyword.ex
@@ -39,4 +39,77 @@ defmodule Example_Ke do
 
     Keyword.get_lazy(keyword, :a, fun)
   end
+
+  def start9 do
+    Keyword.get_values([a: 1, a: 3, a: 6], :a)
+  end
+
+  def start10 do
+    Keyword.merge([a: 1, b: 2], [a: 3, d: 4, a: 5], fn :a, v1, v2 ->
+      v1 + v2
+    end)
+  end
+
+  def start11 do
+    Keyword.new([{:a, 3}, {:c, 5}])
+  end
+
+  def start12 do
+    keyword = [a: 1]
+
+    fun = fn ->
+      13
+    end
+
+    Keyword.pop_lazy(keyword, :a, fun)
+  end
+
+  def start13 do
+    Keyword.pop_values([a: 1, a: 2, a: 3], :a)
+  end
+
+  def start14 do
+    Keyword.put([a: 1, b: 2], :a, 3)
+  end
+
+  def start15 do
+    Keyword.replace([a: 1, b: 2, a: 4], :a, 3)
+  end
+
+  def start16 do
+    Keyword.replace_laz([a: 2, b: 2, a: 2], :a, fn v -> v * 4 end)
+  end
+
+  def start17 do
+    Keyword.split([a: 1, b: 2, c: 3, a: 4], [:a, :c, :e])
+  end
+
+  def start18 do
+    Keyword.take([a: 1, b: 2, c: 3, a: 5], [:a, :c, :e])
+  end
+
+  def start19 do
+    Keyword.to_list(a: 2)
+  end
+
+  def start20 do
+    Keyword.update([a: 12], :b, 15, fn value -> value * 5 end)
+  end
+
+  def start21 do
+    {:ok, result} = Keyword.validate([], [one: 1, two: 2])
+     Enum.sort(result)
+  end
+
+  def start22 do
+    Keyword.validate([three: 3, four: 4], [one: 1, two: 2])
+  end
+
+  def start23 do
+    Keyword.validate!([one: 1], [:one, two: 2]) |> Enum.sort()
+  end
+
+  def start24 do
+    Keyword.values(a: 1, b: 2, a: 3)
+  end
 end
