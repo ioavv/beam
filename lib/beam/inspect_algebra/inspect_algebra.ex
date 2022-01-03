9c7e3d2d559622dcaccf1d021ecc4677daeaab45
Emilano <azadvararvand@gmail.com>
2022-01-03 15:23:11 +0330

    Add files via upload

--- lib/beam/inspect_algebra/inspect_algebra.ex
+++ lib/beam/inspect_algebra/inspect_algebra.ex
@@ -38,4 +38,91 @@ defmodule Inspect_Algebra do
     Inspect.Algebra.format(doc, 80)
   end
 
+  def start8 do
+    inspect_opts = %Inspect.Opts{limit: :infinity}
+    fun = fn i, _opts -> to_string(i) end
+    doc = Inspect.Algebra.container_doc("[", Enum.to_list(1..5), "]", inspect_opts, fun)
+    Inspect.Algebra.format(doc, 5) |> IO.iodata_to_binary()
+  end
+
+  def start9 do
+    inspect_opts = %Inspect.Opts{limit: 3}
+    fun = fn i, _opts -> to_string(i) end
+    opts = [separator: "!"]
+    doc = Inspect.Algebra.container_doc("[", Enum.to_list(1..5), "]", inspect_opts, fun, opts)
+    Inspect.Algebra.format(doc, 20) |> IO.iodata_to_binary()
+  end
+
+  def start10 do
+    docs = ["A", "B", "C"]
+    docs =
+    Inspect.Algebra.fold_doc(docs, fn doc, acc ->
+      Inspect.Algebra.concat([doc, "!", acc])
+    end)
+    Inspect.Algebra.format(docs, 80)
+  end
+
+  def start11 do
+    doc = Inspect.Algebra.glue("hello", " ", "world")
+    doc = Inspect.Algebra.group(doc)
+    doc |> Inspect.Algebra.format(30) |> IO.iodata_to_binary()
+  end
+
+  def start12 do
+    doc = Inspect.Algebra.glue("hello", "\t", "world")
+    Inspect.Algebra.format(doc, 80)
+  end
+
+  def start13 do
+    doc =
+      Inspect.Algebra.group(
+        Inspect.Algebra.concat(
+          Inspect.Algebra.group(
+            Inspect.Algebra.concat(
+              "Hello,",
+              Inspect.Algebra.concat(
+                Inspect.Algebra.break(),
+                "A"
+              )
+            )
+          ),
+          Inspect.Algebra.concat(
+            Inspect.Algebra.break(),
+            "B"
+          )
+        )
+      )
+
+      Inspect.Algebra.format(doc, 80)
+  end
+
+  def start13 do
+    doc =
+      Inspect.Algebra.concat(
+        Inspect.Algebra.concat(
+          "Hughes",
+          Inspect.Algebra.line()
+        ),
+        "Wadler"
+      )
+    Inspect.Algebra.format(doc, 80)
+  end
+
+  def start14 do
+    doc = Inspect.Algebra.nest(Inspect.Algebra.glue("hello", "world"), 5)
+    doc = Inspect.Algebra.group(doc)
+    Inspect.Algebra.format(doc, 5)
+  end
+
+  def start15 do
+    doc = Inspect.Algebra.space("Hughes", "Wadler")
+    Inspect.Algebra.format(doc, 5)
+  end
+
+  def start16 do
+    string = Inspect.Algebra.string("olá")
+    doc = Inspect.Algebra.glue(string, " ", "mundo")
+    doc = Inspect.Algebra.group(doc)
+    Inspect.Algebra.format(doc, 9)
+  end
 end
